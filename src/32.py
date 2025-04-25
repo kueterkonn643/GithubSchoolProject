8724840bdb3ae0c4b72a1bb3a2b3178c257bb22a
kueterkonn643 <kueterkonn643@gmail.com>
2025-04-25 00:38:39 +0000

    Add 32.py

---
+++ src/32.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+data = {
+    'Name': ['John', 'Anna', 'Mike'],
+    'Age': [20, 18, 30],
+    'City': ['New York', 'London', 'Paris']
+}
+
+df = pd.DataFrame(data)
+print(df)
